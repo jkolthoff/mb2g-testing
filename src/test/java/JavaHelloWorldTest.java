22a14aad74c0cbb9444ecd5059752851b6fb4eee
Jan Kolthoff <jan.kolthoff@quinscape.de>
2023-03-17 09:57:09 +0100

    fixed imports

--- src/test/java/JavaHelloWorldTest.java
+++ src/test/java/JavaHelloWorldTest.java
@@ -1,6 +1,6 @@
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 import guru.springframework.JavaHelloWorld;
 

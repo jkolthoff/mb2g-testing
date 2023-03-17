0eac6b28d64ae3a84d4787cef5ea7f4513038109
Jan Kolthoff <jan.kolthoff@quinscape.de>
2023-03-17 09:40:35 +0100

    test method

--- src/test/java/JavaHelloWorldTest.java
+++ src/test/java/JavaHelloWorldTest.java
@@ -1,3 +1,15 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+
+import guru.springframework.JavaHelloWorld;
+
 public class JavaHelloWorldTest {
 
+    @Test
+    public void getHello() {
+        JavaHelloWorld javaHelloWorld = new JavaHelloWorld();
+
+        assertEquals("Hello World", javaHelloWorld.getHello());
+    }
 }
